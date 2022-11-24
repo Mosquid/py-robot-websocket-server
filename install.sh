70eae4ec933f9b3379cbc5fa48b57f68cbe7d919
Dmitry Mosquid <mosquid@gmail.com>
2022-11-24 22:52:05 +0200

    gpiozero added

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install websockets picamera
+pip3 install websockets picamera gpiozero
 
 crontab -l | { cat; echo "@reboot sleep 30 && /usr/bin/python3 $(pwd)/main.py"; } | crontab -
